da816083ebd4b391040c49c60f5a5558dc9d3fad
AkpanElizabeth <ugorjielleza@gmail.com>
2023-02-18 17:24:59 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+  
+
+int main(void)
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+
+}
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-include <stdio.h>
+#include <stdio.h>
 /**
- * main -A program that prints a line with puts function
- *  Return: 0 (success)
- * /
- int main(void)
- {
- puts ("\"programming is like building a multilingual puzzle");
- return (0);
- }
+ *main - print a sentence using puts
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
